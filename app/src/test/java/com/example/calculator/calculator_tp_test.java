0c3f507778a2f109951408ce8278b36c66753ef2
Ho Huu Dat <hohuudat1412@gmail.com>
2025-05-09 08:57:07 +0700

    Test_dau_cong

--- app/src/test/java/com/example/calculator/calculator_tp_test.java
+++ app/src/test/java/com/example/calculator/calculator_tp_test.java
@@ -1,11 +1,14 @@
 package com.example.calculator;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
 public class calculator_tp_test {
+
     @Test
-    public void testDivideWithPrecision() {
+    public void testAdd() {
         calculator_tp calc = new calculator_tp();
-        assertEquals(7.0, calc.divide(5.0, 2.0), 0.0001);
+        assertEquals(7, calc.add(3, 4));  // Kiểm tra 3 + 4 = 7
     }
 }
