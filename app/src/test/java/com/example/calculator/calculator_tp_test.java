183b79682bdb60b2c7d5da9f57ae1ce69f948bc8
Ho Huu Dat <hohuudat1412@gmail.com>
2025-05-09 08:20:54 +0700

    Test thap phan

---
+++ app/src/test/java/com/example/calculator/calculator_tp_test.java
@@ -0,0 +1,11 @@
+package com.example.calculator;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class calculator_tp_test {
+    @Test
+    public void testDivideWithPrecision() {
+        calculator_tp calc = new calculator_tp();
+        assertEquals(2.5, calc.divide(5, 2), 0.0001);
+    }
+}
